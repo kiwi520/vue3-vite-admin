ccb1f6bdbd5a594178250b2e6eed4c81313f490f
russcos <chandle@chandles-iMac.local>
2021-08-08 22:47:10 +0800

    encapsulation axios api/封装axios api

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
-import axios from 'axios'
-import config from './config'
+import request from './utils/request'
 
 import 'element-plus/lib/theme-chalk/index.css'
 
-axios.post(config.mockApi + '/login').then(res => {
-  console.log(res)
-})
-
-createApp(App).use(router).use(ElementPlus).use(store).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$request = request
+app.use(router)
+app.use(ElementPlus)
+app.use(store)
+app.mount('#app')
---
+++ src/utils/request.js
@@ -0,0 +1,78 @@
+/**
+ * axios http封装请求
+ */
+
+import { create } from 'axios'
+import { ElMessage } from 'element-plus'
+import router from '../router'
+
+import config from '../config'
+
+const TOKEN_INVALID = 'token认证失败，请重新登录'
+const NETWORK_ERROR = '网络出错，请稍后重试'
+
+// 创建axios实例对象，添加全局配置
+const http = create({
+  baseURL: config.baseApi,
+  timeout: 8000
+})
+
+// 请求拦截
+http.interceptors.request.use((cf) => {
+  const headers = cf.headers
+  if (!headers.Authorization) headers.Authorization = 'Bear 21424estewt'
+
+  return cf
+})
+
+// 响应拦截
+http.interceptors.response.use((res) => {
+  const { code, msg, data } = res.data
+  if (code === 200) {
+    return data
+  } else if (code === 401) {
+    ElMessage.error(TOKEN_INVALID)
+
+    setTimeout(() => {
+      router.push('/login')
+    }, 1500)
+
+    return Promise.reject(TOKEN_INVALID)
+  } else {
+    ElMessage.error(msg || NETWORK_ERROR)
+    return Promise.reject(msg || NETWORK_ERROR)
+  }
+})
+
+/**
+ * 请求封装函数
+ * @param options
+ */
+function request (options) {
+  options.method = options.method || 'get'
+
+  if (options.method.toLowerCase() === 'get') {
+    options.params = options.data
+  }
+
+  if (config.env === 'prod') {
+    http.defaults.baseURL = config.baseApi
+  } else {
+    http.defaults.baseURL = config.mock ? config.mockApi : config.baseApi
+  }
+
+  http(options)
+}
+
+['get', 'post', 'put', 'delete', 'patch'].forEach(item => {
+  http[item] = (url, data, options) => {
+    return http({
+      url,
+      data,
+      method: item,
+      ...options
+    })
+  }
+})
+
+export default request
